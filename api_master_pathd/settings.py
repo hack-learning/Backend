7fee50e1f6398e028a115abaaa5df4e06acc987a
Dimaps761 <dimaps716@gmail.com>
2020-12-18 21:38:46 -0500

    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')

--- api_master_pathd/settings.py
+++ api_master_pathd/settings.py
@@ -150,9 +150,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-]
